9a655098340cc6e2530a5c01a1bb40b2a8dd893b
T@nvir <u1604090@student.cuet.ac.bd>
2024-10-28 02:36:05 +0600

    desig all services

--- APIGateWay/src/main/java/veer/apigateway/ApiGateWayApplication.java
+++ APIGateWay/src/main/java/veer/apigateway/ApiGateWayApplication.java
@@ -1,14 +1,13 @@
-package backend;
+package veer.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class BackendApplication {
+public class ApiGateWayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(ApiGateWayApplication.class, args);
 	}
 
 }
