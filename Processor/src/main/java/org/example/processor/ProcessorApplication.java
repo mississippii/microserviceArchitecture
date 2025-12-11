32fc9503826b8c6a7f57376b44635dd8cf036359
Tanvir <tanvir@telcobright.com>
2025-12-12 02:17:26 +0600

    add new module

--- Processor/src/main/java/org/example/processor/ProcessorApplication.java
+++ Processor/src/main/java/org/example/processor/ProcessorApplication.java
@@ -2,7 +2,6 @@ package org.example.processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProcessorApplication {
